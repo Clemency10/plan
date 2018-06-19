156a37bb76e4895c87b331d50d8bb951be29e042
Chew Jia Ying Jamie <>
2018-06-19 11:12:13 +0800

    Added Accordion Edited Numeric Up Down Control Function, Range Slider, Button (now clickable) Adjusted width of Multi-select Dropdown Box

--- js/plan.js
+++ js/plan.js
@@ -1,33 +1,125 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#sideNav'
+    });
 
 })(jQuery); // End of use strict
 
 // initialize all tooltips
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
 })
+
+//slider
+
+$(document).ready(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+
+});
+
+// accordion
+
+$(document).ready(function () {
+        $('.ui.accordion')
+            .accordion();
+    }
+);
+
+// ui dropdown
+
+$(document).ready(function () {
+        $('.ui.dropdown')
+            .dropdown();
+    }
+);
+
+//slider value
+
+$(document).ready(function () {
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+    }
+})
+
+
+// slider range
+
+$(document).ready(function () {
+    $("#slider-range").slider();
+});
+
+
+// numerical up down
+
+// Constants
+var KEY_UP = 38,
+    KEY_DOWN = 40;
+
+// Variables
+var min = 0,
+    max = 30,
+    step = 1;
+
+$('.ui.icon.button').click(function () {
+    var command = $(this).attr('command');
+    HandleUpDown(command);
+});
+
+$('#txtNum').keypress(function (e) {
+    var code = e.keyCode;
+    if (code != KEY_UP && code != KEY_DOWN) return;
+    var command = code == KEY_UP ? 'Up' : code == KEY_DOWN ? 'Down' : '';
+    HandleUpDown(command);
+});
+
+function HandleUpDown(command) {
+    var val = $('#txtNum').val().trim();
+    var num = val !== '' ? parseInt(val) : 0;
+
+    switch (command) {
+        case 'Up':
+            if (num < max) num += step;
+            break;
+        case 'Down':
+            if (num > min) num -= step;
+            break;
+    }
+
+    $('#txtNum').val(num);
+}
